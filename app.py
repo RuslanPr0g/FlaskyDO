5a27d3cf6f932bc84c7419c833105f209d3f5576
RuslanPr0g <59767834+RuslanPr0g@users.noreply.github.com>
2020-06-28 14:45:54 +0300

    styles fixed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -16,9 +16,21 @@ class TODO(db.Model):
         return '<task %r>' % self.id
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = TODO(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Error.. Please, try one more time.'
+    else:
+        tasks = TODO.query.order_by(TODO.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
 
 if __name__ == "__main__":
